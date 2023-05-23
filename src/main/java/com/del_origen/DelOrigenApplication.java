6f776ed42357c66294d786b0f40124972de5be71
K-BRIEL <cristiangabrielmesiano@gmail.com>
2023-05-23 19:32:36 -0300

    modificaciones server

--- src/main/java/com/del_origen/DelOrigenApplication.java
+++ src/main/java/com/del_origen/DelOrigenApplication.java
@@ -2,12 +2,29 @@ package com.del_origen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+/*
 @SpringBootApplication
-public class DelOrigenApplication {
+public class DelOrigenApplication extends {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DelOrigenApplication.class, args);
 	}
 
+} */
+
+
+ @SpringBootApplication
+public class DelOrigenApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelOrigenApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+	return application.sources(DelOrigenApplication.class);
+	}
+
 }
+
